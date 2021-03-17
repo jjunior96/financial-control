eb4bed97cbbc58b503bd80b3cf79403a4a0be46a
Junior Alves <jjalves96@gmail.com>
2021-03-17 19:24:38 -0300

    chore: add new styles

--- src/templates/Dashboard/styles.ts
+++ src/templates/Dashboard/styles.ts
@@ -1,3 +1,13 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 1140px;
+
+  margin: 0 auto;
+  padding: 0 1.6rem;
+`;
