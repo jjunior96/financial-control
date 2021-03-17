ad034cb14025435b269427e156c1c675ead3b51d
Junior Alves <jjalves96@gmail.com>
2021-03-17 19:24:19 -0300

    chore: import Summary component

--- src/templates/Dashboard/index.tsx
+++ src/templates/Dashboard/index.tsx
@@ -1,10 +1,14 @@
 import Header from 'components/Header';
+import Summary from 'components/Summary';
 import * as S from './styles';
 
 const Dashboard: React.FC = () => {
   return (
     <S.Container>
       <Header />
+      <S.Content>
+        <Summary />
+      </S.Content>
     </S.Container>
   );
 };
