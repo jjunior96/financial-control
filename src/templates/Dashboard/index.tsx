26ead601f398d1da531aa4d1c3f91671c6441c0a
Junior Alves <jjalves96@gmail.com>
2021-03-18 18:13:31 -0300

    feat: create Transaction Modal

--- src/templates/Dashboard/index.tsx
+++ src/templates/Dashboard/index.tsx
@@ -1,15 +1,46 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+
 import Header from 'components/Header';
 import Summary from 'components/Summary';
 import TransactionsTable from 'components/TransactionsTable';
 import * as S from './styles';
 
+/**
+ * Como, ao abrir o modal, ele fica por cima da tela inteira,
+ * devemos ocultar os elementos que estão dentro do elemento raíz.
+ * Por questão de acessibilidade, passamos para o
+ * Modal, qual é o elemento root, que no caso do Next.js
+ * "#__next"
+ */
+Modal.setAppElement('#__next');
+
 const Dashboard: React.FC = () => {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(
+    false
+  );
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <S.Container>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <S.Content>
         <Summary />
         <TransactionsTable />
+
+        <Modal
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+        >
+          <h2>Cadastrar Transação</h2>
+        </Modal>
       </S.Content>
     </S.Container>
   );
