1d8a16612e6ff4df7f7fa49307501df9df24b067
Junior Alves <jjalves96@gmail.com>
2021-03-18 19:27:02 -0300

    chore: add content Modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,7 @@
 import Modal from 'react-modal';
+import { RiCloseFill as CloseButton } from 'react-icons/ri';
 
-// import * as S from './styles';
+import * as S from './styles';
 
 type NewTransactionModalProps = {
   isOpen: boolean;
@@ -12,8 +13,23 @@ const NewTransactionModal = ({
   onRequestClose
 }: NewTransactionModalProps) => {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Cadastrar Transação</h2>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay" // Sobrescreve os styles default
+      className="react-modal-content" // Classe para estilização do conteúdo do modal
+    >
+      <button type="button" onClick={onRequestClose}>
+        <CloseButton size={26} />
+      </button>
+      <S.Container>
+        <h2>Cadastrar Transação</h2>
+
+        <input type="text" placeholder="Titulo" />
+        <input type="number" placeholder="Valor" />
+        <input type="text" placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </S.Container>
     </Modal>
   );
 };
